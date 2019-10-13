c90bebd1f41b0d39a1be400194ee76cbe567b39c
JasperMertens <jasper.mertens@gmail.com>
2019-10-13 19:41:02 +0200

    Working initialization

--- init.sql
+++ init.sql
@@ -1,18 +1,18 @@
 /* can't run this through python, dot commands are cli-only */
 
 /* init tables */
-create table if not exists nw_tx (id integer, date string,
+create table if not exists nw_tx (id text primary key,
+	            date text,
                     amount integer, balance integer,
-	            currency string, name string,
-	            account string, statement string,
-	            category string, description string);
+	            currency text, name text,
+	            account text, statement text,
+	            category text, description text);
 
 /* Import the data */
-.separator ";"
-.mode csv
-/* . import ../Formatted_data/2019/09/Argenta_BE02973118990540_2019-09-27_233911.csv nw_tx */
+.separator ';'
+.import ../Formatted_data/2019/09/Argenta_BE02973118990540_2019-09-27_233911.csv nw_tx
 
-.import glob '../Formatted_data/2019/09/*.csv' nw_tx
+/*.import glob '../Formatted_data/2019/09/*.csv' nw_tx*/
 
 /*.read categorise.sql*/
 
--- query-balance.sql
+++ query-balance.sql
@@ -1,12 +1,14 @@
 
 .print ''
 .print 'overall profit/loss by month'
-select strftime('%Y-%m', date),SUM(amount) 
+/*select strftime('%Y-%m', date),SUM(amount) 
     from ( select date, amount, description from cc_tx
             UNION
             select date, amount, description from nw_tx 
             UNION
-            select date, amount, description from fcc_tx )
+            select date, amount, description from fcc_tx )*/
+select strftime('%Y-%m', date),SUM(amount) 
+    from ( select date, amount, description from nw_tx
     GROUP BY date(date, 'start of month')
     ORDER BY date(date, 'start of month') DESC;
 .print ''
